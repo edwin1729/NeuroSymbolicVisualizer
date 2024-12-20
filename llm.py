430970973ca12a4601ca9dae205d7b237a2b817f
Edwin Fernando <ef322@ic.ac.uk>
2024-12-20 12:03:00 +0000

    feat: testing different llms

---
+++ llm.py
@@ -0,0 +1,12 @@
+from transformers import pipeline
+
+question_answerer = pipeline(task="question-answering", model="deepset/roberta-base-squad2")
+
+preds = question_answerer(
+    question="What is the name of the repository?",
+    context="The name of the repository is huggingface/transformers",
+)
+
+print(
+    f"score: {round(preds['score'], 4)}, start: {preds['start']}, end: {preds['end']}, answer: {preds['answer']}"
+)
